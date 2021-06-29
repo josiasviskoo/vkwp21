6dda598c04746b2e49e9a0fe14216e6c54dd891e
Josias <josiasviskoo@gmail.com>
2021-06-29 19:26:50 -0300

    apagar temas antigos

--- wp-content/themes/VKWP208/vk-info-top.php
+++
@@ -1,9 +0,0 @@
-<div class="uk-text-center" style="min-height: 30px; color: white; background-color: #0f0f0f;">
-	<div class="uk-container">
-			contato@viskoo.com.br 
-			<a href="https://www.instagram.com/agencia.viskoo" uk-icon="icon: instagram" style="color: white; margin-left: 2px; margin-right: 2px;"></a>
-			<a href="https://www.facebook.com/viskoopropaganda" uk-icon="icon: facebook" style="color: white; margin-left: 2px; margin-right: 2px;"></a>
-			<a href="https://api.whatsapp.com/send?phone=5516993438187&text=Gostaria%20de%20falar%20sobre%20meu%20projeto... estava na loja virtual" uk-icon="icon: whatsapp" style="color: white; margin-left: 2px; margin-right: 2px;"></a>
-			(16) 99343.8187
-	</div>
-</div>
