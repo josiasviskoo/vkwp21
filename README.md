fb12939c54777ca9abed9d8baba022dc3103c3b5
Josias <josiasviskoo@gmail.com>
2021-07-28 16:33:36 -0300

    uikit header

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ vkwp21-official
 
 http://vkwp21.viskoo-cloud-server.com.br/
 
-git add . && git commit -m "sabado hair" && git push
+git add . && git commit -m "." && git push
--- wp-content/themes/VKWP21/header.php
+++ wp-content/themes/VKWP21/header.php
@@ -34,8 +34,6 @@
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/js/uikit.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/js/uikit-icons.min.js"></script>
 
-
-
         <?php wp_head(); ?>
     </head>
   </head>
