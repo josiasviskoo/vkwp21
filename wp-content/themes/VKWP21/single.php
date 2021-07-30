e1ac9003ccad5db97a322cbd0835c5015bfb332a
Josias <josiasviskoo@gmail.com>
2021-07-30 13:37:41 -0300

    update

--- wp-content/themes/VKWP21/single.php
+++ wp-content/themes/VKWP21/single.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 
     <main class="produtos-recimec">
-      
+
       <div class="container">
         <div class="col-xs-12 col-sm-12 col-md-12">
-          <?php get_template_part('_detalhe'); ?>
+          <?php //get_template_part('_detalhe'); ?>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-3" style="display:none">
           <?php get_sidebar(); ?>
